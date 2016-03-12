6c4cd861e5cebd6ee209411c9f7bee5e4afe62f3
John Turner <fenwayriffs@gmail.com>
2016-03-12 16:15:18 -0500

    removed all conditionals in Factory, implementing full polymorphism with Dictionary refs

--- Factory/CustomerFactory.cs
+++ Factory/CustomerFactory.cs
@@ -8,22 +8,18 @@ namespace FactoryCustomer
 {
 	public static class Factory
 	{
-		//private static Dictionary<string, CustomerBase> custs = 
-		//	new Dictionary<string, CustomerBase> ();
+		private static Dictionary<string, CustomerBase> custs = 
+			new Dictionary<string, CustomerBase> ();
 		
 		public Factory ()
 		{
-		//	custs.Add ("Customer", new Customer ());
-		//	custs.Add ("Lead", new Lead ());
+			custs.Add ("Customer", new Customer ());
+			custs.Add ("Lead", new Lead ());
 		}
 
 		public static CustomerBase Create(string TypeCust)
 		{
-			if (TypeCust == "Customer") {
-				return = new Customer ();
-			} else {
-				return = new Lead ();
-			}
+			return custs [TypeCust];
 		}
 	}
 }
